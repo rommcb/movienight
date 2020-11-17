fc31fced7ae1935deaf1e892f6578de4761cfac7
Romain Borremans <romain.borremans@gmail.com>
2020-11-17 11:50:23 +0100

    it's not the homepage but the routes :D

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,15 @@
 Rails.application.routes.draw do
-
-  # get 'event_subscriptions/create'
-  # get 'reviews/create'
-  # get 'preferences/edit'
-  # get 'preferences/update'
-  # get 'movies/index'
-  # get 'movies/show'
-  # get 'movies/show_winner'
-  # get 'events/index'
-  # get 'events/new'
-  # get 'events/create'
-  # get 'events/show'
-  # get 'events/edit'
-  # get 'events/update'
-  # get 'events/destroy'
-
-
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :event_subscriptions, only: [:create]
-  resources :reviews, only: [:create]
-  resources :preferences, only: [:edit, :update]
-  resources :events
-  resources :movies, only: [:index, :show]
-  # otherxise, it doesn't appear in the routes.
-  get 'movies/show_winner'
 
+  resources :events do 
+    resources :show_winner, only: [:index]
+  end
+
+  resources :event_subscriptions, only: [:create]
+  resources :preferences, only: [:edit, :update]
+  resources :reviews, only: [:create]
+  resources :movies, only: [:index, :show]
 
 end
