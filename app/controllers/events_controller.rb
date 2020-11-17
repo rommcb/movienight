465256d0e5da0bbe09a18058dc34ce698dd171ad
pierre crochet <pierrecrochet03@gmail.com>
2020-11-17 17:40:31 +0100

    add the movie like to e tester

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,4 +19,17 @@ class EventsController < ApplicationController
 
   def destroy
   end
+
+  def find_movie_like
+    # what we want is  an array of movies wih score > 0
+
+    # get the score in event_movies and sort the score > 0
+    # @array_score = Event_movies.select { |movie| movie.score.positive? }
+    # @array_score = Event_movies.where(:score.positive?)
+    # store them into an array as an array of object of movies
+    @array_score = EventMovie.where("score.positive?")
+
+    # return the array of object
+
+  end
 end
