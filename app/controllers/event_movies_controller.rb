9c18d1e534ae6cc4174d1b6a7efa33c8fea95477
FDeleval <frederic.deleval@student.uclouvain.be>
2020-11-18 16:02:55 +0100

    swipe implem

--- app/controllers/event_movies_controller.rb
+++ app/controllers/event_movies_controller.rb
@@ -5,12 +5,21 @@ class EventMoviesController < ApplicationController
   end
 
   def update
-    @movie = EventMovie.find(params[:id])
-      if params[:commit] == "like"
-      @movie.score += 1
-      else
-      @movie.score -= 1
-      end
-    @movie.save
+    # @movie = EventMovie.find(params[:id])
+    #   if params[:commit] == "like"
+    #   @movie.score += 1
+    #   else
+    #   @movie.score -= 1
+    #   end
+    # @movie.save
+    event_movie = EventMovie.find(params[:event_movie][:id])
+    if params[:like] != nil
+      event_movie.score += 1
+      event_movie.save!
+    end
+    review = Review.new(event_movie_id: event_movie.id, user_id:current_user.id)
+    review.save!
+    redirect_to("/events/swipe/1")
   end
+
 end
