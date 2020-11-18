58969dc59616b05d3bbed3a1def20e3344b473fe
FDeleval <frederic.deleval@student.uclouvain.be>
2020-11-18 12:47:31 +0100

    stuff

--- app/controllers/event_subscriptions_controller.rb
+++ app/controllers/event_subscriptions_controller.rb
@@ -8,13 +8,13 @@ class EventSubscriptionsController < ApplicationController
   def _code(code)
     event = Event.where(code: code).first
     if event == nil
-      pp "Wrong code"
+      redirect_to(events_path("Error"))
     else
       if EventSubscription.where(user_id: current_user.id).length == 0
         event_sub = EventSubscription.new(owner: false, user_id: current_user.id, event_id: event.id)
         event_sub.save!
       end
-      redirect_to(events_path(15)) ## should redirect to the event that was subscribed to
+      redirect_to("/events/#{event.id}") ## should redirect to the event that was subscribed to
     end
   end
   def destroy
