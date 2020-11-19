3381cfd3ec15e89cf7f3b147b4a91f2b0ec417e5
Romain Borremans <romain.borremans@gmail.com>
2020-11-19 12:34:20 +0100

    Sign in / up logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
 
-  root to: 'pages#home'
+
+  # get '/user' => "", :as => :user_root
+
+  
+  
+
+  devise_scope :user do 
+    authenticated :user do
+      root 'events#index', as: :authenticated_root 
+    end 
+    unauthenticated do 
+      root to: 'pages#home', as: :unauthenticated_root 
+    end 
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
 
