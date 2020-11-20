1dd39992b60e23ae5ea62eb4818b72f613a52969
FDeleval <frederic.deleval@student.uclouvain.be>
2020-11-20 15:45:01 +0100

    qsdfqsdfqsdfqsdfqsdfqsfqsdfqsdf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
