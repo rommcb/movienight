bbe03581b6f93efc164e0b87168b39449ae04a54
Romain Borremans <romain.borremans@gmail.com>
2020-11-23 14:34:37 +0100

    fixed swipes counter

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
   has_many :event_movies, dependent: :destroy
   has_many :event_subscriptions, dependent: :destroy
   has_many :users, through: :event_subscriptions
+  has_many :reviews, through: :event_movies
 end
