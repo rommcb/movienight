f45ce314d3684ddcbc25e0132e0b128be319cd2e
FDeleval <frederic.deleval@student.uclouvain.be>
2020-11-25 09:43:07 +0100

    fixed

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -29,36 +29,42 @@ class PreferencesController < ApplicationController
     if actors.length != 0
       PreferencesActor.where(user_id: current_user.id).destroy_all
       actors = actors[1..]
+      actors.each do |actor_name|
+        actor_id = Actor.where(fullname: actor_name).first.id
+        pa = PreferencesActor.new(user_id: current_user.id, actor_id:actor_id)
+        pa.save!
+      end
+    else
+      PreferencesActor.where(user_id: current_user.id).destroy_all
     end
+
     genres = params[:genre_list].split(",")
     if genres.length != 0
       PreferencesGenre.where(user_id: current_user.id).destroy_all
       genres = genres[1..]
+      genres.each do |genre_name|
+        genre_id = Genre.where(name: genre_name).first.id
+        pg = PreferencesGenre.new(user_id: current_user.id, genre_id:genre_id)
+        pg.save!
+      end
+    else
+      PreferencesGenre.where(user_id: current_user.id).destroy_all
     end
+
     directors = params[:director_list].split(",")
     if directors.length != 0
       PreferencesDirector.where(user_id: current_user.id).destroy_all
       directors = directors[1..]
+      directors.each do |director_name|
+        director_id = Director.where(fullname: director_name).first.id
+        pd = PreferencesDirector.new(user_id: current_user.id, director_id:director_id)
+        pd.save!
+      end
+    else
+      PreferencesDirector.where(user_id: current_user.id).destroy_all
     end
     duration = params[:duration].to_i
 
-    actors.each do |actor_name|
-      actor_id = Actor.where(fullname: actor_name).first.id
-      pa = PreferencesActor.new(user_id: current_user.id, actor_id:actor_id)
-      pa.save!
-    end
-
-    genres.each do |genre_name|
-      genre_id = Genre.where(name: genre_name).first.id
-      pg = PreferencesGenre.new(user_id: current_user.id, genre_id:genre_id)
-      pg.save!
-    end
-
-    directors.each do |director_name|
-      director_id = Director.where(fullname: director_name).first.id
-      pd = PreferencesDirector.new(user_id: current_user.id, director_id:director_id)
-      pd.save!
-    end
 
     if duration == 0
        current_user.max_duration_pref = 600
