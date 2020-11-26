c923eca5d0f2dbbc0eef9f3e39ee6fb8446027dd
FDeleval <frederic.deleval@student.uclouvain.be>
2020-11-26 10:50:33 +0100

    freddd

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,7 +20,7 @@ require("channels")
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
-// ----------------------------------------------------
+// ----------------------------------------------------d
 
 // External imports
 import "bootstrap";
